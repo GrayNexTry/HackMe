16d5a23170548863162e42ad3f86eb8ded489174
NexTry <www.nextry@gmail.com>
2023-11-23 22:58:38 +0300

    HackMe v1
    HackMe release

---
+++ HackMe 1.0.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <stdlib.h>
+
+
+static std::string PASS = ")D3g_23Gh";
+
+bool check_passwor(std::string pass){
+	for (size_t i = 0; i < PASS.length(); i++)
+	{
+		if (pass[i] != PASS[i]) return false;
+	}
+	return true;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::string enterLogin;
+	std::string enterPass;
+
+	while (true)
+	{
+		std::cout << "Введите логин:" << "\t";
+		std::cin >> enterLogin;
+		std::cout << "Введите пароль:" << "\t";
+		std::cin >> enterPass;
+		if (enterLogin[0] > 54 && enterLogin[0] < 91 && check_passwor(enterPass))
+		{
+			break;
+		}
+		else
+		{
+			system("cls");
+			std::cout << "Не верный логин или пароль :(" << "\n";
+		}
+	}
+	std::cout << "Все верно!" << "\n";
+	system("PAUSE");
+}
